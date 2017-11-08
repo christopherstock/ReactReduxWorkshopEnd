21f950cd801490978e92b9a16b080f3f9f1ec1d8
Christopher Stock <email@christopherstock.de>
2017-11-08 07:20:38 +0100

    Altered indention.

--- js/index.jsx
+++ js/index.jsx
@@ -1,29 +1,29 @@
 
-    // specify the application title
-    const APPLICATION_TITLE = "React Task List";
+// specify the application title
+const APPLICATION_TITLE = "React Task List";
 
-    // set page title
-    document.title = APPLICATION_TITLE;
+// set page title
+document.title = APPLICATION_TITLE;
 
-    // reference the main container
-    let mainContainer = document.getElementById( "mainContainer" );
+// reference the main container
+let mainContainer = document.getElementById( "mainContainer" );
 
-    // create redux store
-    let store = Redux.createStore( Reducer.globalReducer );
+// create redux store
+let store = Redux.createStore( Reducer.globalReducer );
 
-    store.dispatch( Action.createTask( "Müll rausbringen" ) );
-    store.dispatch( Action.createTask( "Abwaschen"        ) );
-    store.dispatch( Action.createTask( "Wäsche waschen"   ) );
+store.dispatch( Action.createTask( "Müll rausbringen" ) );
+store.dispatch( Action.createTask( "Abwaschen"        ) );
+store.dispatch( Action.createTask( "Wäsche waschen"   ) );
 
-    // render the App component into the main container
-    ReactDOM.render(
+// render the App component into the main container
+ReactDOM.render(
 
-        <ReactRedux.Provider store={ store }>
+    <ReactRedux.Provider store={ store }>
 
-            <App
-                title={ APPLICATION_TITLE }
-            />
+        <App
+            title={ APPLICATION_TITLE }
+        />
 
-        </ReactRedux.Provider>,
-        mainContainer
-    );
+    </ReactRedux.Provider>,
+    mainContainer
+);
