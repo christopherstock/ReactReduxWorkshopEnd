11276e585952568a81c867305592f3a8ebe236ce
Christopher Stock <email@christopherstock.de>
2017-11-10 09:05:31 +0100

    Added more direct dispatchers after store creation.

--- js/index.jsx
+++ js/index.jsx
@@ -13,6 +13,8 @@ let store = Redux.createStore( Reducer.globalReducer );
 store.dispatch( Action.createTask( "Müll rausbringen" ) );
 store.dispatch( Action.createTask( "Abwaschen"        ) );
 store.dispatch( Action.createTask( "Wäsche waschen"   ) );
+store.dispatch( Action.moveTaskUp( 2 ) );
+store.dispatch( Action.deleteTask( 0 ) );
 
 // render the App component into the main container
 ReactDOM.render(
